f6aed314052721adc09ddd37452bf5df2872f1dc
seo2730 <seo273072@gmail.com>
2021-05-16 13:11:12 +0900

    backup

--- ros_drone.m
+++ ros_drone.m
@@ -4,59 +4,79 @@
 % rosinit('192.168.0.35');
 
 %% Global Variables
-global r; global wn;
-global x; global y; global z;
-global flag; global cnt;
-global pos_pub; global vel_pub; global pos_sub
+global ref_x; global ref_y; global ref_z;
+global vel_pub; global pos_sub
 global ref_x; global ref_y; global ref_z;
 global x; global y; global z;
+
+global x; global y; global z;
 global data_saved_x; global data_saved_y; global data_saved_z;
+global err_pre_x; global err_pre_y; global err_pre_z;
+global cnt_t;
+
+global position_controller_x;
+global position_controller_y;
+global position_controller_z;
+
+global tstart; global tend;
+
+global timer_t;
 
 %% Subscriber
-pos_sub = rossubscriber('drone1/ground_truth_to_tf/pose','geometry_msgs/PoseStamped',@sub_test_callback);
-
+% pos_sub = rossubscriber('drone1/ground_truth_to_tf/pose','geometry_msgs/PoseStamped',@sub_test_callback);
+pos_sub = rossubscriber('drone1/ground_truth_to_tf/pose','geometry_msgs/PoseStamped');
+tstart = tic;
+pause(1);
+    
 msg2 = receive(pos_sub,10);
+x = msg2.Pose.Position.X;
+y = msg2.Pose.Position.Y;
+z = msg2.Pose.Position.Z;
+
+cnt_t=cnt_t+1;
+data_saved_x(cnt_t,:) = x;
+data_saved_y(cnt_t,:) = y;
+data_saved_z(cnt_t,:) = z;
 
 %% SetPoint Command
-% pos = rosmessage(pos_pub);
-% pos.Pose.Position.X = 10;
-% pos.Pose.Position.Y = 10;
-% pos.Pose.Position.Z = 10;
+ref_x = 4;
+ref_y = 4;
+ref_z = 6;
 
-x = -10;
-y = -10;
-z = 10;
+position_controller_x = (ref_x-x);
+position_controller_y = (ref_y-y);
+position_controller_z = (ref_z-z);
+
+% position_controller_x = 2*(ref_x-x) + (ref_x-x-err_pre_x)/timer_t;
+% position_controller_y = 2*(ref_y-y) + (ref_y-y-err_pre_y)/timer_t;
+% position_controller_z = (ref_z-z) + (ref_z-z-err_pre_z)/timer_t;
 
 % 지정한 변수값들을 Vector3Stamped message에 넣어주는 코드
 vel = rosmessage(vel_pub);
-vel.Linear.X = 2*(x-ref_x);
-vel.Linear.Y = 2*(y-ref_y);
-vel.Linear.Z = (z-ref_z);
+   
+vel.Linear.X = position_controller_x;  
+vel.Linear.Y = position_controller_y;   
+vel.Linear.Z = position_controller_z;
 
+err_pre_x = ref_x-x;
+err_pre_y = ref_y-y;
+err_pre_z = ref_z-z;
 %% Send Setpoint
-%while(1)
-%sp.Vector.X = sp.Vector.X + 1;
-%sp.Vector.Y = sp.Vector.Y + 1;
-
-% send(pos_pub,pos);
 send(vel_pub,vel);
-
-
-
-% send(command,sc);
+tend = toc(tstart);
 %% Callback function
-function sub_test_callback(src,msg2)
-    global ref_x; global ref_y; global ref_z;
-    global data_saved_x; global data_saved_y; global data_saved_z;
-    global cnt_t;
-    ref_x = msg2.Pose.Position.X;
-    ref_y = msg2.Pose.Position.Y;
-    ref_z = msg2.Pose.Position.Z;
-    
-    cnt_t=cnt_t+1;
-    data_saved_x(cnt_t,:) = ref_x;
-    data_saved_y(cnt_t,:) = ref_y;
-    data_saved_z(cnt_t,:) = ref_z;
-end
+% function sub_test_callback(~,msg2)
+%     global x; global y; global z;
+%     global data_saved_x; global data_saved_y; global data_saved_z;
+%     global cnt_t;
+%     x = msg2.Pose.Position.X;
+%     y = msg2.Pose.Position.Y;
+%     z = msg2.Pose.Position.Z;
+%     
+%     cnt_t=cnt_t+1;
+%     data_saved_x(cnt_t,:) = x;
+%     data_saved_y(cnt_t,:) = y;
+%     data_saved_z(cnt_t,:) = z;
+% end
 
 
--- ros_timer.m
+++ ros_timer.m
@@ -1,3 +1,6 @@
+clc
+clear all
+
 %% Global Variables
 global x; global y; global z;
 global flag; global cnt;
@@ -5,15 +8,17 @@ global pos_pub; global vel_pub; global pos_sub;
 global x; global y; global z;
 global timer_t; global cnt_t;
 global data_saved_x; global data_saved_y; global data_saved_z;
+global err_pre_x; global err_pre_y; global err_pre_z;
+global tstart; global tend;
+tstart = 0; tend = 0;
 cnt_t = 0;
-timer_t = 0.01;
+timer_t = 0.001;
 re_f=0;
 flag=0;
 %% Connect Through Wifi
 rosshutdown;
 % ros UDP 접속을 위한 상대 companion computer ip
-rosinit('192.168.0.17');
-
+rosinit('192.168.0.18');
 
 %% Publisher
 
@@ -24,7 +29,9 @@ vel_pub = rospublisher('drone1/cmd_vel','geometry_msgs/Twist');
 % command = rospublisher('ros/attitude_command','geometry_msgs/Quaternion');
 
 x=0; y=0; z=0;
-
+err_pre_x = 0;
+err_pre_y = 0;
+err_pre_z = 0;
 
 %%
 t1 = timer;
@@ -35,3 +42,7 @@ t1.ExecutionMode = 'fixedSpacing';
 % t.ExecutionMode = 'fixedDelay';
 
 start(t1);
+
+%%
+t=0:timer_t:timer_t*cnt_t-timer_t;
+plot(t,data_saved_x)
